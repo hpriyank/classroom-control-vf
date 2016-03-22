91b33ac0a9f10ef0851f5b60dd36618d4d217024
hpriyank <bujji3282@gmail.com>
2016-03-22 09:05:54 +0530

    7.2exercise

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,7 +37,6 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-
 node default {
 # This is where you can declare classes for all nodes.
 # Example:
@@ -55,3 +54,4 @@ path => '/usr/bin:/usr/local/bin',
 creates => '/etc/motd',
 }
 }
+
