1a817b8762aa1bc2564faf64bd5ef9e9c7387ae0
hpriyank <bujji3282@gmail.com>
2016-03-22 09:22:07 +0530

    7.2.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,7 +49,7 @@ notify { "Hello, my name is ${::hostname}": }
 # mode => '0644',
 # content => "Today I learned what it means to manage state using Puppet.\n",
 #}
-exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+exec { "Welcome":
 path => '/usr/bin:/usr/local/bin',
 creates => '/etc/motd',
 }
