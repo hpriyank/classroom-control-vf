28cea0413f698dc605f04e1e10de06e6465af947
hpriyank <bujji3282@gmail.com>
2016-03-22 14:32:18 +0530

    re1

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,5 +1,18 @@
-PORT="11211"
-USER="memcached"
-MAXCONN="96"
-CACHESIZE="32"
-OPTIONS=""
+class memcached {
+package { 'memcached':
+ensure => present,
+}
+file { '/etc/sysconfig/memcached':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/memcached/memcached',
+require => Package['memcached'],
+}
+service { 'memcached':
+ensure => running,
+enable => true,
+subscribe => File['/etc/sysconfig/memcached'],
+}
+} 
