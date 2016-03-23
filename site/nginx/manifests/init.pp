ae2cbd9910288ba6347dd37f9b7fd2d2f211be44
hpriyank <bujji3282@gmail.com>
2016-03-23 10:38:59 +0530

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,46 +1,55 @@
 class nginx {
-  package { 'nginx':
-    ensure => present,
-  }
-  file { '/var/www':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0775',
-  }
-  file { '/var/www/index.html':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0664',
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-  file { '/etc/nginx/conf.d':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0775',
-  }
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
-    source  => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-  }
+
+File{
+group => root,
+owner => root,
+mode => '0664',
+}
+
+package {'nginx':
+ensure => present,
+}
+
+$docroot='/var/www';
+
+#document root /var/www
+file {$docroot:
+ensure => directory,
+}
+
+
+#index.html
+file{"${docroot}/index.html":
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+
+#config file nginx.conf
+file {'/etc/nginx/nginx.conf':
+ensure => file,
+
+source => 'puppet:///modules/nginx/nginx.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+
+file { '/etc/nginx/conf.d':
+ensure => directory,
+owner => 'root',
+group => 'root',
+mode => '0775',
+}
+
+#config default.conf
+file {'/etc/nginx/conf.d/default.conf':
+ensure => file,
+source => 'puppet:///modules/nginx/default.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+
+service { 'nginx':
+ensure => running,
+enable => true,
+}
 }
